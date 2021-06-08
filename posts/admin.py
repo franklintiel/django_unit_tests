78e9b2b07941f3558238c78eb23983a1e6e2df38
Pedro Caraballo <pcaraballo@solucionescalamari.com>
2021-06-07 20:55:35 -0500

    adding test for model app Posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'text_content', 'published_date', 'state')
+    ordering = ('-state',)
+
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text_content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    state = models.BooleanField(default=True)
+    # image...
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return u'{0} - ({1})'.format(self.user, self.published_date)
+
