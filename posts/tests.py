e7bd4ecea59c22893ff7e82f8dd25260e94157ac
randy salas herrera <rsalas@lsv-tech.com>
2021-06-09 15:39:18 -0500

    subiendo test

--- posts/tests.py
+++ posts/tests.py
@@ -1,28 +1,83 @@
-from django.test import TestCase
+from unittest import TestCase
+
+from rest_framework.authtoken.admin import User
+
+from posts.post import Posts
 
 
 class PostTestCase(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create(username='admin2', first_name='John2',
+                                        last_name='Doe2', email='info2@domain.com')
+        self.user.set_password('admin123')
+        self.user.save()
+
+    def tearDown(self):
+        if self.user != None:
+            self.user.delete()
+
+    def get_posts_class(self):
+        return Posts()
+
     def test_create_post_success(self):
-        pass
+        post = self.get_posts_class()
+        get_post = post.create_post(title='titulo',
+                                    content='contenido',
+                                    creator=self.user)
+        self.assertTrue(get_post)
 
     def test_create_post_invalid_fields(self):
-        pass
+        post = self.get_posts_class()
+        get_post = post.create_post(title=None,
+                                    content='contenido',
+                                    creator=self.user)
+        self.assertFalse(get_post)
 
     def test_update_post(self):
-        pass
+        post = self.get_posts_class()
+        post.create_post(title='titulo',
+                         content='contenido',
+                         creator=self.user)
+
+        update_post = post.update_post(title='titulo', new_title='titulo cambiado')
+        self.assertTrue(update_post)
 
     def test_update_post_invalid_fields(self):
-        pass
+        post = self.get_posts_class()
+        post.create_post(title='probando',
+                         content='admin123',
+                         creator=self.user)
+
+        update_post = post.update_post(title='probando', new_title=None)
+        self.assertFalse(update_post)
 
     def test_update_post_not_found(self):
-        pass
+        post = self.get_posts_class()
+        post.create_post(title='probando',
+                         content='admin123',
+                         creator=self.user)
+
+        update_post = post.update_post(title='titulo', new_title='actualizar titulo')
+        self.assertFalse(update_post)
 
     def test_delete_post(self):
-        pass
+        post = self.get_posts_class()
+        post.create_post(title='probando',
+                         content='admin123',
+                         creator=self.user)
+
+        delete_post = post.delete_post(title='probando')
+        self.assertTrue(delete_post)
 
     def test_delete_post_not_found(self):
-        pass
+        post = self.get_posts_class()
+        post.create_post(title='probando',
+                         content='admin123',
+                         creator=self.user)
+
+        delete_post = post.delete_post(title='titulo')
+        self.assertFalse(delete_post)
 
     def test_publish_post(self):
         pass
