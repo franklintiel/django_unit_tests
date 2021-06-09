22fdee6e91e0d8c5d1d5c2db7fcd58b57f35da4d
JhonGarcia <jgarcia@lsv-tech.com>
2021-06-09 11:01:52 -0500

    Updating test

--- posts/models.py
+++ posts/models.py
@@ -4,31 +4,31 @@ import datetime
 
 class Created_Update_Class(models.Model):
     created_at = models.DateField(auto_now_add = True, editable= False)
-    update_at = models.DateField(auto_now = True)
+    update_at = models.DateField(auto_now = True , editable= True)
 
     class Meta:
         abstract = True
 
 class Posts(Created_Update_Class):
-    id = models.IntegerField(auto_created = True,primary_key = True)
+    #id = models.IntegerField(auto_created = True,primary_key = True, editable= False)
     title = models.CharField(max_length=150)
     content = models.CharField(max_length=300)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Publication(Created_Update_Class):
-    id = models.IntegerField(auto_created=True, primary_key=True)
+    #id = models.IntegerField(auto_created=True, primary_key=True,editable= False)
     start_at = models.DateField(null=False, editable= True)
     end_at = models.DateField(null=False, editable= True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Comment(Created_Update_Class):
-    id = models.IntegerField(auto_created=True, primary_key=True)
+    #id = models.IntegerField(auto_created=True, primary_key=True, editable= False)
     post = models.ForeignKey(Posts, on_delete=models.CASCADE)
     comment = models.CharField(max_length=150)
     response = models.CharField(max_length=150)
 
 class Image(Created_Update_Class):
-    id = models.IntegerField(auto_created=True, primary_key=True)
+    #id = models.IntegerField(auto_created=True, primary_key=True, editable= False)
     post = models.ForeignKey(Posts, on_delete=models.CASCADE)
     image = models.FileField(null=True, blank=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ posts/tests_template.py
@@ -0,0 +1,247 @@
+from django.test import TestCase
+from .models import Posts
+from django.contrib.auth.models import User
+
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        #Set up a model of type Post to be used in the tests
+        #Previouly, Create the the instance of an User
+
+        self.user = User.objects.create(username="TestUser")
+        self.post = Posts.objects.create(title= "My first post",
+                                         content= "it post is used to practice Django test Lesson",
+                                         creator= self.user)
+        #import pdb;  pdb.set_trace()
+        #post = Posts() ; post.save()
+
+    def tearDown(self):
+        if self.post != None or self.user != None:
+            self.post.delete()
+
+        #import pdb;  pdb.set_trace()
+
+    def test_create_post_success(self):
+        new_post = Posts.objects.create(title="My first post",
+                                         content="it post is used to practice Django test Lesson",
+                                         creator=self.user)
+        self.assertEqual(new_post,self.post)
+
+    def test_create_post_invalid_fields(self):
+        pass
+
+    def test_update_post(self):
+        pass
+
+    def test_update_post_invalid_fields(self):
+        pass
+
+    def test_update_post_not_found(self):
+        pass
+
+    def test_delete_post(self):
+        pass
+
+    def test_delete_post_not_found(self):
+        pass
+
+    def test_publish_post(self):
+        pass
+
+    def test_publish_post_not_found(self):
+        pass
+
+    def test_publish_post_already_published(self):
+        pass
+
+    def test_lock_post(self):
+        pass
+
+    def test_lock_post_already_locked(self):
+        pass
+
+    def test_lock_post_not_found(self):
+        pass
+
+    def test_close_post(self):
+        pass
+
+    def test_close_post_not_found(self):
+        pass
+
+    def test_close_post_already_closed(self):
+        pass
+
+    def test_list_posts(self):
+        pass
+
+    def test_list_posts_empty(self):
+        pass
+
+    def test_publish_post_list(self):
+        pass
+
+    def test_publish_post_empty(self):
+        pass
+
+    def test_get_post(self):
+        pass
+
+    def test_get_post_not_found(self):
+        pass
+
+    def test_create_comments_success(self):
+        pass
+
+    def test_create_comments_post_not_found(self):
+        pass
+
+    def test_create_comments_invalid_fields(self):
+        pass
+
+    def test_response_comments(self):
+        pass
+
+    def test_response_comments_post_not_found(self):
+        pass
+
+    def test_response_comments_not_found(self):
+        pass
+
+    def test_list_publication_dates_by_post(self):
+        pass
+
+    def test_list_publication_dates_empty_by_post(self):
+        pass
+
+    def test_list_publication_dates_by_post_not_found(self):
+        pass
+
+    def test_create_publication_dates(self):
+        pass
+
+    def test_create_publication_dates_invalid_fields(self):
+        pass
+
+    def test_create_publication_dates_invalid_date_fields(self):
+        pass
+
+    def test_create_publication_dates_post_not_found(self):
+        pass
+
+    def test_update_publication_dates(self):
+        pass
+
+    def test_update_publication_dates_not_found(self):
+        pass
+
+    def test_update_publication_dates_post_not_found(self):
+        pass
+
+    def test_update_publication_dates_invalid_fields(self):
+        pass
+
+    def test_update_publication_dates_invalid_date_fields(self):
+        pass
+
+    def test_delete_publication_dates(self):
+        pass
+
+    def test_delete_publication_dates_not_found(self):
+        pass
+
+    def test_delete_publication_dates_post_not_found(self):
+        pass
+
+    def test_active_publication_dates(self):
+        pass
+
+    def test_active_publication_dates_not_found(self):
+        pass
+
+    def test_active_publication_dates_post_not_found(self):
+        pass
+
+    def test_active_publication_dates_already_active(self):
+        pass
+
+    def test_stop_publication_dates(self):
+        pass
+
+    def test_stop_publication_dates_not_found(self):
+        pass
+
+    def test_stop_publication_dates_post_not_found(self):
+        pass
+
+    def test_stop_publication_dates_already_active(self):
+        pass
+
+    def test_close_publication_dates(self):
+        pass
+
+    def test_close_publication_dates_not_found(self):
+        pass
+
+    def test_close_publication_dates_post_not_found(self):
+        pass
+
+    def test_close_publication_dates_already_active(self):
+        pass
+
+    def test_create_publication_after_create_post(self):
+        pass
+
+    def test_disable_post_on_unavailable_publication_access(self):
+        pass
+
+    def test_disable_post_on_available_publication_access(self):
+        pass
+
+    def test_validate_post_before_publish(self):
+        pass
+
+    def test_validate_post_before_publish_invalid(self):
+        pass
+
+    def test_validate_post_before_publish_invalid_not_dates(self):
+        pass
+
+    def test_validate_post_before_publish_invalid_without_images(self):
+        pass
+
+    def test_accept_only_images_png(self):
+        pass
+
+    def test_accept_only_images_invalid_extension(self):
+        pass
+
+    def test_upload_only_limit_images_by_post(self):
+        pass
+
+    def test_upload_only_limit_images_by_post_invalid(self):
+        pass
+
+    def test_create_image_from_post(self):
+        pass
+
+    def test_create_image_from_post_not_found(self):
+        pass
+
+    def test_create_image_from_post_invalid_fields(self):
+        pass
+
+    def test_delete_image_from_post(self):
+        pass
+
+    def test_delete_image_from_post_image_not_found(self):
+        pass
+
+    def test_delete_image_from_post_not_found(self):
+        pass
+
+    def test_creator_information_displayed_on_api(self):
+        pass
+
+    def test_creator_information_not_displayed_on_api(self):
+        pass
